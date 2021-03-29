8cb96d1af2bea7e3f2583f4eba7c6f2da530ab80
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-03-29 20:42:29 +0530

    Added: java script to print prime factors

---
+++ ForLoop/Prime_Factorization.js
@@ -0,0 +1,20 @@
+function prime_factors(num) {
+    function is_prime(num) {
+      for (let i = 2; i <= Math.sqrt(num); i++)
+      {
+        if (num % i === 0) return false;
+      }
+      return true;
+    }
+    const result = [];
+    for (let i = 2; i <= num; i++)
+    {
+      while (is_prime(i) && num % i === 0) 
+      {
+        if (!result.includes(i)) result.push(i);
+        num /= i;
+      }
+    }
+    return result;
+  }
+  console.log(prime_factors(33));
