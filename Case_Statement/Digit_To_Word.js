08fa312029c69f5e33836cdad4b625a76f982a97
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-03-29 15:23:52 +0530

    Added: java script to display word when given number

---
+++ Case_Statement/Digit_To_Word.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')();
+
+const number = prompt('Enter number');
+
+switch (number) {
+    case "1": console.log("One");
+        break;
+    case "2": console.log("Two");
+        break;
+    case "3": console.log("Three");
+        break;
+    case "4": console.log("Four");
+        break;
+    case "5": console.log("Five");
+        break;
+    case "6": console.log("Six");
+        break;
+    case "7": console.log("Seven");
+        break;
+    case "8": console.log("Eight");
+        break;
+    case "9": console.log("Nine");
+        break;
+}
