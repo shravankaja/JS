16d8c779e1eb4ee13ba716e6e70356519f2cfc36
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-03-29 14:17:11 +0530

    Added: java script to read a number and convert it into word

---
+++ If_Else/Read_Number_ConverTo_Word.js
@@ -0,0 +1,34 @@
+const prompt = require('prompt-sync')();
+
+const num = prompt('Enter number');
+
+if(num == 1) {
+    console.log("One");
+}
+else if(num == 2) {
+    console.log("Two");
+}
+else if(num == 3) {
+    console.log("Three");
+}
+else if(num == 4) {
+    console.log("Four");
+}
+else if(num == 5) {
+    console.log("Five");
+}
+else if(num == 6) {
+    console.log("Six");
+}
+else if(num == 7) {
+    console.log("Seven");
+}
+else if(num == 8) {
+    console.log("Eight");
+}
+else if(num == 9 ) {
+    console.log("Nine");
+}
+else {
+    console.log("Enter single digit number only")
+}
